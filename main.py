b491393d3ac5f173f4fbb550464e448d5ff2b725
Aparajita Singh <aparajitalnctscse@gmail.com>
2023-11-11 23:50:51 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+from tkinter import *
+import math
+# ---------------------------- CONSTANTS ------------------------------- #
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+TEXT = "✔"
+reps = 0
+timer= None
+# ---------------------------- TIMER RESET ------------------------------- #
+
+
+# ---------------------------- TIMER MECHANISM ------------------------------- # 
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+
+# ---------------------------- UI SETUP ------------------------------- #
