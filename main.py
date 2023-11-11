213800e13d07e6db2faa9e7e57a991a63761ceb2
Aparajita Singh <aparajitalnctscse@gmail.com>
2023-11-11 23:54:31 +0530

    UI Setup

--- main.py
+++ main.py
@@ -56,3 +56,33 @@ def count_down(count):
         tick.config(text = mark)
 
 # ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Pomodoro")
+window.config(padx=100, pady=50, bg=YELLOW)
+
+canvas = Canvas(width=200, height=224,  bg= YELLOW, highlightthickness=0)
+tomato_img = PhotoImage(file="tomato.png")
+
+
+canvas.create_image(100, 112, image=tomato_img)
+timer_text = canvas.create_text(100, 127, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.grid(column =1, row= 1)
+
+
+timer_label = Label(text="Timer", foreground=GREEN, background=YELLOW, font=(FONT_NAME, 35, "bold"))
+timer_label.grid(column = 1, row=0)
+
+start_button = Button(text="Start", background= "white", width=5, command=start_timer)
+start_button.grid(column=0, row=2)
+
+reset_button = Button(text="Reset", background= "white", width=5, command=reset_timer)
+reset_button.grid(column=2, row=2)
+
+tick = Label( foreground=GREEN, background=YELLOW)
+tick.grid(column=1, row=3)
+
+
+
+
+
+window.mainloop()
