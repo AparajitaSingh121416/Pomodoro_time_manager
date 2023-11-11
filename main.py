0131aa0430548215ebdb8fa01d5e614792316564
Aparajita Singh <aparajitalnctscse@gmail.com>
2023-11-11 23:52:44 +0530

    added timer mechanism

--- main.py
+++ main.py
@@ -22,6 +22,21 @@ def reset_timer():
     reps = 0
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
+def start_timer():
+    global reps
+    reps += 1
+    work_sec= WORK_MIN*60
+    short_break_sec = SHORT_BREAK_MIN*60
+    long_break_sec = LONG_BREAK_MIN*60
+    if reps % 8== 0:
+        count_down(long_break_sec)
+        timer_label.config(text="Long Break", foreground=PINK)
+    elif reps % 2 == 0:
+        count_down(short_break_sec)
+        timer_label.config(text="Short Break", foreground=RED)
+    else:
+        count_down(work_sec)
+        timer_label.config(text="Work", foreground=GREEN)
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
